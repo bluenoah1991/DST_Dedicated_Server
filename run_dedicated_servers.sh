24e40071cbbbf3a838df6f03d722c0648a77fe9a
Evan <codemeow@icloud.com>
2019-03-23 17:06:38 +0800

    Update run_dedicated_servers.sh

--- run_dedicated_servers.sh
+++ run_dedicated_servers.sh
@@ -26,6 +26,7 @@ check_for_file "$dontstarve_dir/$cluster_name/cluster_token.txt"
 check_for_file "$dontstarve_dir/$cluster_name/Master/server.ini"
 check_for_file "$dontstarve_dir/$cluster_name/Caves/server.ini"
 
+# ./steamcmd.sh +force_install_dir "$install_dir" +login <username> <password> +app_update 343050 validate +quit
 ./steamcmd.sh +force_install_dir "$install_dir" +login anonymous +app_update 343050 validate +quit
 
 check_for_file "$install_dir/bin"
